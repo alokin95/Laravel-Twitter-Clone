1ae62a80b6f69a2f6bda1398a6ce3a2fa55fd2ae
Nikola <nikola_m95@hotmail.com>
2018-06-29 01:09:40 +0200

    Minor changes

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -1,88 +1,38 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Comment;
+
 use App\Tweet;
 
 class CommentsController extends Controller
 {
 
-    private $data = [];
-
-
-    public function index($id = null)
+    public function __construct()
     {
-        //
-        $this->data['comments'] = Comment::with('tweet', 'user')->get();
-        $this->data['tweets'] = Tweet::all();
 
-        if (isset($id)) {
-            $this->data['single'] = Comment::find($id);
-        }
-
-        return view('admin.comments', $this->data);
+        $this->middleware('auth');
     }
 
-    public function store(Request $request)
+
+    public function store(Tweet $tweet)
     {
-        //
-        $validated = $request->validate([
-        'comment' => 'required',
-        'tweet' => 'required|not_in:0'
-        ]);
 
         try {
-            $comm = Comment::create([
-            'body' => $validated['comment'],
-            'tweet_id' => $validated['tweet'],
-            'user_id' => auth()->user()->id
+            $comment = request()->validate([
+            'comment' => 'required|min:5|max:50'
             ]);
 
-            \Log::info(auth()->user()->name . " has commented through admin panel. Comment: " . $comm->body . ", TweetID: " . $comm->tweet_id);
-            return redirect()->back();
-            } catch (\Illuminate\Database\QueryException $ex) {
-                \Log::error($ex->getMessage());
-            return redirect()->back();
+            $comment = $tweet->addComment($comment['comment']);
+
+            return $comment;
+            \Log::info('Comment added by user ' . auth()->user()->email);
         }
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-        $validated = $request->validate([
-        'comment' => 'required',
-        'tweet' => 'required|not_in:0'
-        ]);
-
-        try {
-            $comment = Comment::find($id);
-
-            $comment->body = $validated['comment'];
-            $comment->tweet_id = $validated['tweet'];
-
-            $comment->save();
-
-            \Log::info(auth()->user()->name . " has updated through admin panel. Comment: " . $comment->body . ", TweetID: " . $comment->tweet_id);
-            return redirect()->back();
-        } catch (\Illuminate\Database\QueryException $ex) {
+        catch (\Illuminate\Database\QueryException $ex) {
             \Log::error($ex->getMessage());
             return redirect()->back();
         }
-    }
 
-    public function destroy($id)
-    {
-        //
-        try {
-            $comm = Comment::destroy($id);
-            \Log::info(auth()->user()->name . " has deleted comment ");
-            return redirect()->back();
-        } catch (\Illuminate\Database\QueryException $ex) {
-            \Log::error($ex->getMessage());
-            return redirect()->back();
-        }
     }
 }
